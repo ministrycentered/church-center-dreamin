7788c067ff52ef69d9a4bd7b97aa171405f150bb
Jonathan Suh <hello@jonsuh.com>
2017-12-22 11:34:02 -0600

    KeyboardAvoidingView to prevent keyboard from covering input

--- modules/LocationScreen.js
+++ modules/LocationScreen.js
@@ -9,7 +9,9 @@ import {
   Text,
   Button,
   TextInput,
-  FlatList
+  FlatList,
+  Keyboard,
+  KeyboardAvoidingView,
 } from "react-native";
 
 export class LocationScreen extends React.Component {
@@ -19,118 +21,121 @@ export class LocationScreen extends React.Component {
 
   render() {
       return (
-        <View
+        <KeyboardAvoidingView
+          behavior="position"
           style={{
             flex: 1,
             alignItems: "center",
             backgroundColor: "#fff",
           }}>
-          <View
-            style={{
-              height: 300,
-              alignItems: "center",
-              justifyContent: "flex-end",
-            }}>
+            <View
+              style={{
+                height: 300,
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}>
+              <View
+                style={{
+                  alignItems: "center",
+                  marginBottom: 16,
+                }}>
+                <Ionicons
+                  name="ios-pin-outline"
+                  color="#61ba66"
+                  size={80}
+                  style={{
+                    backgroundColor: "transparent"
+                  }}
+                />
+                <View
+                  style={{
+                    backgroundColor: "#00000010",
+                    width: 50,
+                    height: 50,
+                    borderRadius: 50,
+                    position: "absolute",
+                    bottom: 0,
+                    transform: [
+                      { scaleY: 0.2 },
+                      { translateY: 42 },
+                    ],
+                    zIndex: -1,
+                  }}>
+                </View>
+              </View>
+            </View>
             <View
               style={{
                 alignItems: "center",
-                marginBottom: 16,
+                maxWidth: 220,
               }}>
-              <Ionicons
-                name="ios-pin-outline"
-                color="#61ba66"
-                size={80}
-                style={{
-                  backgroundColor: "transparent"
-                }}
-              />
-              <View
-                style={{
-                  backgroundColor: "#00000010",
-                  width: 50,
-                  height: 50,
-                  borderRadius: 50,
-                  position: "absolute",
-                  bottom: 0,
-                  transform: [
-                    { scaleY: 0.2 },
-                    { translateY: 42 },
-                  ],
-                  zIndex: -1,
-                }}>
-              </View>
-            </View>
-          </View>
-          <View
-            style={{
-              alignItems: "center",
-              maxWidth: 220,
-            }}>
-            <Text
-              style={{
-                fontSize: 16,
-                fontWeight: "bold",
-                color: "#585756",
-                textAlign: "center",
-                marginBottom: 8,
-              }}>
-              Find Your Church
-            </Text>
-            <Text
-              style={{
-                color: "#878685",
-                textAlign: "center",
-              }}
-            >
-              Church Center uses your location to search for nearby churches.
-            </Text>
-            <View
-              style={{
-                marginTop: 32
-              }}>
-              <TextInput
-                placeholder="Enter zip code"
-                style={{
-                  color: "#878685",
-                  padding: 16,
-                  borderWidth: 1,
-                  borderColor: "#ddd",
-                  borderStyle: "solid",
-                  borderRadius: 3,
-                }}
-              />
               <Text
                 style={{
-                  color: "#87868550",
-                  marginTop: 8,
+                  fontSize: 16,
+                  fontWeight: "bold",
+                  color: "#585756",
+                  textAlign: "center",
                   marginBottom: 8,
-                  textAlign: "center"
                 }}>
-                or
+                Find Your Church
+              </Text>
+              <Text
+                style={{
+                  color: "#878685",
+                  textAlign: "center",
+                }}
+              >
+                Church Center uses your location to search for nearby churches.
               </Text>
               <View
                 style={{
-                  flexDirection: "row"
+                  marginTop: 32
                 }}>
-                <FontAwesome
-                  name="location-arrow"
-                  color="#007aff"
-                  size={16}
+                <TextInput
+                  placeholder="Enter zip code"
                   style={{
-                    backgroundColor: "transparent",
-                    marginRight: 8,
+                    color: "#878685",
+                    padding: 16,
+                    borderWidth: 1,
+                    borderColor: "#ddd",
+                    borderStyle: "solid",
+                    borderRadius: 3,
                   }}
+                  keyboardType="numeric"
+                  returnKeyType="done"
                 />
                 <Text
                   style={{
-                    color: "#007aff"
+                    color: "#87868550",
+                    marginTop: 8,
+                    marginBottom: 8,
+                    textAlign: "center"
                   }}>
-                  Use current location
+                  or
                 </Text>
+                <View
+                  style={{
+                    flexDirection: "row"
+                  }}>
+                  <FontAwesome
+                    name="location-arrow"
+                    color="#007aff"
+                    size={16}
+                    style={{
+                      backgroundColor: "transparent",
+                      marginRight: 8,
+                    }}
+                  />
+                  <Text
+                    style={{
+                      color: "#007aff"
+                    }}>
+                    Use current location
+                  </Text>
+                </View>
               </View>
             </View>
-          </View>
-        </View>
+        </KeyboardAvoidingView>
       )
   }
 }
