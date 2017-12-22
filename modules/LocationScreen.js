64a55396135e6db5b02722e029eeab510b922915
Jonathan Suh <hello@jonsuh.com>
2017-12-22 11:08:39 -0600

    Zip and current location.

--- modules/LocationScreen.js
+++ modules/LocationScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Avatar, Icon } from 'react-native-elements';
 import Ionicons from "react-native-vector-icons/Ionicons";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 import {
   StyleSheet,
@@ -21,11 +22,12 @@ export class LocationScreen extends React.Component {
         <View
           style={{
             flex: 1,
+            alignItems: "center",
             backgroundColor: "#fff",
           }}>
           <View
             style={{
-              height: 350,
+              height: 300,
               alignItems: "center",
               justifyContent: "flex-end",
             }}>
@@ -36,7 +38,7 @@ export class LocationScreen extends React.Component {
               }}>
               <Ionicons
                 name="ios-pin-outline"
-                color="#007aff"
+                color="#61ba66"
                 size={80}
                 style={{
                   backgroundColor: "transparent"
@@ -44,7 +46,7 @@ export class LocationScreen extends React.Component {
               />
               <View
                 style={{
-                  backgroundColor: "#00000020",
+                  backgroundColor: "#00000010",
                   width: 50,
                   height: 50,
                   borderRadius: 50,
@@ -58,26 +60,74 @@ export class LocationScreen extends React.Component {
                 }}>
               </View>
             </View>
-            <View>
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontWeight: "bold",
-                  color: "#585756",
-                  textAlign: "center",
-                  marginBottom: 8,
-                }}>
-                Find Your Church
-              </Text>
-              <Text
+          </View>
+          <View
+            style={{
+              alignItems: "center",
+              maxWidth: 220,
+            }}>
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: "bold",
+                color: "#585756",
+                textAlign: "center",
+                marginBottom: 8,
+              }}>
+              Find Your Church
+            </Text>
+            <Text
+              style={{
+                color: "#878685",
+                textAlign: "center",
+              }}
+            >
+              Church Center uses your location to search for nearby churches.
+            </Text>
+            <View
+              style={{
+                marginTop: 32
+              }}>
+              <TextInput
+                placeholder="Enter zip code"
                 style={{
                   color: "#878685",
-                  textAlign: "center",
-                  maxWidth: 220,
+                  padding: 16,
+                  borderWidth: 1,
+                  borderColor: "#ddd",
+                  borderStyle: "solid",
+                  borderRadius: 3,
                 }}
-              >
-                Church Center uses your location to search for nearby churches.
+              />
+              <Text
+                style={{
+                  color: "#87868550",
+                  marginTop: 8,
+                  marginBottom: 8,
+                  textAlign: "center"
+                }}>
+                or
               </Text>
+              <View
+                style={{
+                  flexDirection: "row"
+                }}>
+                <FontAwesome
+                  name="location-arrow"
+                  color="#007aff"
+                  size={16}
+                  style={{
+                    backgroundColor: "transparent",
+                    marginRight: 8,
+                  }}
+                />
+                <Text
+                  style={{
+                    color: "#007aff"
+                  }}>
+                  Use current location
+                </Text>
+              </View>
             </View>
           </View>
         </View>
